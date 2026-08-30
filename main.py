b57c7cb2bdaa09eb35e0f360b80cd7f8b3d54a2e
mrityuanjay07 <mrityuanjaykewat@gmail.com>
2026-08-30 21:36:03 +0530

    addind a sorting method in my project

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path,HTTPException,Query
 import json
 app = FastAPI()
 
@@ -19,3 +19,21 @@ def about():
 def view():
     data = load_data()
     return data
+
+@app.get("/patient_id/{patient_id}")
+def patient_id(patient_id: str = Path(..., description = 'Enter the patient id to get the details of the patient', example = "P001")):
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404,detail='patient not found')
+
+@app.get("/sort")
+def sort_patients(sort_by: str = Query(..., description = 'Enter the field to sort the patients on the basis of height, weight, or bmi'), order:str = Query('asc', description = 'Enter the order of sorting, either asc or desc')):
+    data = load_data()
+    if sort_by not in ['height', 'weight', 'bmi']:
+        raise HTTPException(status_code= 400, detail= 'invalid sort field, please choose from height, weight or bmi')
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code = 400, detail= 'invalid order, please choose either asc or desc')
+    sorted_data = sorted(data.values(), key=lambda x: x[sort_by], reverse=(order == 'desc'))
+    return sorted_data
