49a7959e3235d1fd2eae12f087be7ab2f8fbd69b
mrityuanjay07 <mrityuanjaykewat@gmail.com>
2026-09-01 20:57:02 +0530

    datavalidation by pydantic

--- pydantic_model.py
+++ pydantic_model.py
@@ -1,12 +1,16 @@
-from pydantic import BaseModel
+
+
+from pydantic import BaseModel, EmailStr, AnyUrl,Field
 
 class patient(BaseModel):
-    name: str
+    name: str = Field(min_length=3, max_length=50)
+    email: EmailStr
+    linkdin: AnyUrl
     city: str
-    age: int
+    age: int = Field(gt=0,lt=120)
     gender: str
-    height: float
-    weight: float
+    height: float= Field(gt=0)
+    weight: float = Field(gt=0)
     bmi: float
     verdict: str
 
@@ -30,6 +34,6 @@ def update_patient(patient: patient):
     print(patient.bmi)
     print(patient.verdict)
 
-patient_info = {'name': 'mrityuanjay kewat', 'city': 'mumbai', 'age': 30, 'gender': 'male', 'height': 175.0, 'weight': 70.0, 'bmi': 22.86, 'verdict': 'healthy'}
+patient_info = {'name': 'mrityuanjay kewat','email': 'abc@gmail.com','linkdin':'https://www.linkedin.com/in/mrityuanjay-kewat', 'city': 'mumbai', 'age': 30, 'gender': 'male', 'height': 175.0, 'weight': 70.0, 'bmi': 22.86, 'verdict': 'healthy'}
 patient1 = patient(**patient_info)
 insert_patient(patient1)
