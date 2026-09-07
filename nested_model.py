f23dcecd143f75cc806160b35eda6d6a4cc12086
mrityuanjay07 <mrityuanjaykewat@gmail.com>
2026-09-07 21:10:48 +0530

    learning the concept of nested model in pydantic

---
+++ nested_model.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+
+class Address(BaseModel):
+    street:str
+    city:str
+    state:str
+    pincode:str
+
+class patient(BaseModel):
+    name: str
+    age: int
+    address: Address
+
+address_dict={
+    'street':'beemagram',
+    'city':'kotma',
+    'state':'mp',
+    'pincode':'481701'
+}
+address1 = Address(**address_dict)
+
+patient_dict = {
+    'name':'anku',
+    'age': 19, 
+    'address':address1  
+}
+patient1 = patient(**patient_dict)
+print(patient1)
+print(patient1.name)
+print(patient1.address.city)
+print(patient1.address.pincode)
