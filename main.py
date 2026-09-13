ad6de89d976fed7691d4bcb686ca3b30b893f297
Mrityuanjay  Kumar kewat <mrityuanjaykewat@gmail.com>
2026-09-14 00:19:22 +0530

    sucessfully correcting bugs in update end point

--- main.py
+++ main.py
@@ -5,31 +5,39 @@ from pydantic import BaseModel,Field,computed_field
 from typing import Literal, Optional,Dict,List,Annotated
 import json
 
-from serialization import patient
+
 app = FastAPI()
 
 class Patient(BaseModel):
     id: Annotated[str, Field(min_length=3, max_length=10, title="Patient ID", description="This field is required and should be between 3 and 10 characters",example="P001")]
     name: Annotated[str, Field(min_length=2, max_length=100, title="Patient Name", description="This field is required and should be between 2 and 100 characters",example="John Doe")]
+    city: Annotated[str, Field(min_length=2, max_length=100, title="Patient City", description="This field is required and should be between 2 and 100 characters",example="New York")]
     age: Annotated[int, Field(gt=0, title="Patient Age", description="This field is required and should be a positive integer",example=30)]
     height: Annotated[float, Field(gt=0, title="Patient Height", description="This field is required and should be a positive float",example=175.5)]
     weight: Annotated[float, Field(gt=0, title="Patient Weight", description="This field is required and should be a positive float",example=70.0)]
-  
-class patientUpdate(BaseModel):
-    name: Annotated[Optional [str],Field(default=None)]
-    age:Annotated[Optional[int],Field(defautl=None)]
+
+class patientupdate(BaseModel):
+    name: Annotated[Optional[str], Field(default = None)]
+    city:Annotated[Optional[str],Field(default=None)]
+    age:Annotated[Optional[int],Field(default=None)]
     gender:Annotated[Optional[Literal['male','female']],Field(default=None)]
     height:Annotated[Optional[float],Field(default=None)]
     weight:Annotated[Optional[float],Field(default=None)]
+  
+
 
     @computed_field
     @property
     def bmi(self) -> float:
+        if self.height is None or self.weight is None:
+            return None
         bmi = round(self.weight/((self.height/100)**2),2)
         return bmi
     @computed_field
     @property
     def verdict(self) -> str:
+        if self.bmi is None:
+            return "Invalid BMI"
         if self.bmi < 18.5:
             return "Underweight"
         elif 18.5 <= self.bmi < 24.9:
@@ -91,27 +99,39 @@ def create_patient(patient: Patient):
 
     return JSONResponse(status_code=201, content={'message': 'patient created successfully'})
 
-@app.put('/edit/{patient_id}')
-def patient_update(patient_id: str, patient_update:patientUpdate):
-
+@app.put('/update/{patient_id}')
+def update_patient(patient_id:str, patient_update: patientupdate):
     data = load_data()
- 
+
     if patient_id not in data:
-       raise HTTPException(status_code= 404, detail='patient not found')
+        raise HTTPException(status_code = 404, detail='patient not found')
 
     existing_patient_info = data[patient_id]
-    updated_patient_info=patient_update.model_dump(exclude_unset=True)
+
+    updated_patient_info = patient_update.model_dump(exclude_unset=True)
 
     for key, value in updated_patient_info.items():
-        existing_patient_info[key]= value
+        existing_patient_info[key] = value
 
-        data[patient_id] = existing_patient_info
-
-    existing_patient_info['id']= patient_id
-    patient_pydantic_obj= patient(**existing_patient_info)
+    existing_patient_info['id'] = patient_id
+    patient_pydantic_obj = Patient(**existing_patient_info)
 
     existing_patient_info = patient_pydantic_obj.model_dump(exclude='id')
 
     data[patient_id] = existing_patient_info
-
     save_data(data)
+
+    return JSONResponse(status_code=200, content={'message': 'patient updated successfully'})
+
+# @app.delete('/delete/{patient_id}')
+# def delete_patient(patient_id:str):
+#     data = load_data()
+
+#     if patient_id not in data:
+#         raise HTTPException(status_code=404, detail='patient not found')
+
+#     del data[patient_id]
+
+#     save_data(data)
+
+#     return JSONResponse(status_code=200, content={'message':'patient deleted sucessfully'})
